818f75ae73babd489be0b50bc475388b84b83388
animeduniya <duniyabarbaad24@gmail.com>
2025-03-03 14:54:29 +0530

    Create ProfileScreen.js

---
+++ frontend/src/screens/ProfileScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ProfileScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>👤 User Profile</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#f8f8f8' },
+  title: { fontSize: 20, fontWeight: 'bold' }
+});
+
+export default ProfileScreen;
